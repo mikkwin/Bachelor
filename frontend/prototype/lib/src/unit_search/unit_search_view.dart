1eaa0b4a781f106fcdcdcc342466871e15035e1c
mikkwin <mikkel.winther.personal@gmail.com>
2024-11-05 06:55:10 +0100

    Navigation fix on login and view devices

--- frontend/prototype/lib/src/unit_search/unit_search_view.dart
+++ frontend/prototype/lib/src/unit_search/unit_search_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prototype/src/login/login_view.dart';
 import 'package:prototype/src/unit_page/unit_page_view.dart';
 
 /// Displays a list of SampleItems.
@@ -6,92 +7,96 @@ class UnitSearchView extends StatelessWidget {
   const UnitSearchView({super.key});
 
   static const routeName = '/unit_search';
-  
+
   @override
   Widget build(BuildContext context) {
-
     double screenWidth = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Device Search'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text("Please input device ID"), // Potententiel fremtidig tjek: Søg efter device
-            const SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.2),
-              child: const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'ID',
-                )
-              )
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const UnitPageView())
-                    );
-              }, 
-              child: const Text("Search")
-            ),
-            const SizedBox(height: 20),
-            Text(
-              "History", 
-              style: TextStyle(
-                fontSize: screenWidth * 0.06,
-              )),
-            SizedBox(
-              height: 400, // Set a fixed height for the ListView
-              child: ListView(
-                children: const <Widget>[
-                  ListTile(
-                    leading: Icon(Icons.car_crash),
-                    title: Text("Device: 123456789012345"),
-                    trailing: Icon(Icons.search),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.car_crash),
-                    title: Text("Device: 123456789012345"),
-                    trailing: Icon(Icons.search),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.car_crash),
-                    title: Text("Device: 123456789012345"),
-                    trailing: Icon(Icons.search),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.car_crash),
-                    title: Text("Device: 123456789012345"),
-                    trailing: Icon(Icons.search),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.car_crash),
-                    title: Text("Device: 123456789012345"),
-                    trailing: Icon(Icons.search),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.car_crash),
-                    title: Text("Device: 123456789012345"),
-                    trailing: Icon(Icons.search),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.car_crash),
-                    title: Text("Device: 123456789012345"),
-                    trailing: Icon(Icons.search),
-                  ),
-                  // Add more ListTile entries here if needed
-                ],
+    return PopScope(
+        onPopInvokedWithResult: (didPop, result) {
+          if (didPop) {
+            Future.delayed(Duration.zero, () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const LoginView()));
+            });
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Device Search'),
+          ),
+          body: Center(
+              child: Column(
+            children: [
+              const Text(
+                  "Please input device ID"), // Potententiel fremtidig tjek: Søg efter device
+              const SizedBox(height: 20),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.2),
+                  child: const TextField(
+                      decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'ID',
+                  ))),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const UnitPageView()));
+                  },
+                  child: const Text("Search")),
+              const SizedBox(height: 20),
+              Text("History",
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.06,
+                  )),
+              SizedBox(
+                height: 400, // Set a fixed height for the ListView
+                child: ListView(
+                  children: const <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.car_crash),
+                      title: Text("Device: 123456789012345"),
+                      trailing: Icon(Icons.search),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.car_crash),
+                      title: Text("Device: 123456789012345"),
+                      trailing: Icon(Icons.search),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.car_crash),
+                      title: Text("Device: 123456789012345"),
+                      trailing: Icon(Icons.search),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.car_crash),
+                      title: Text("Device: 123456789012345"),
+                      trailing: Icon(Icons.search),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.car_crash),
+                      title: Text("Device: 123456789012345"),
+                      trailing: Icon(Icons.search),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.car_crash),
+                      title: Text("Device: 123456789012345"),
+                      trailing: Icon(Icons.search),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.car_crash),
+                      title: Text("Device: 123456789012345"),
+                      trailing: Icon(Icons.search),
+                    ),
+                    // Add more ListTile entries here if needed
+                  ],
+                ),
               ),
-            ),
-          ],
-        )
-      ),
-    );
+            ],
+          )),
+        ));
   }
 }
